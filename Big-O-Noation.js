5e6605d4efb88ba8b176094b85ef65384d480aec
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-11 15:31:41 +0100

    Create Big-O-Noation.js

---
+++ Big-O-Noation.js
@@ -0,0 +1,18 @@
+function linearFunc(arr) {
+  for (let i = 0; i < arr.length; i++) { //O(n) => linear 
+    console.log(10000 * 10000000) //constant  O(1) => A constant is a step that doesn't scale with the input
+  }
+}
+const arr = [1, 2, 3, 4, 5, 6]
+linearFunc(arr)
+
+//O(n^2)
+function square(n) {
+  for (let i = 0; i < n; i++) {
+    for let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+square(4)
+  
