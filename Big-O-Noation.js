e3b4f2e65e1d4371bbefbe77d4f6dece97828c54
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-11 15:56:47 +0100

    Update Big-O-Noation.js

--- Big-O-Noation.js
+++ Big-O-Noation.js
@@ -9,7 +9,7 @@ linearFunc(arr)
 //O(n^2)
 function square(n) {
   for (let i = 0; i < n; i++) {
-    for let j = 0; j < n; j++) {
+    for (let j = 0; j < n; j++) {
       console.log(i, j);
     }
   }
