3853987e3ac6629d9e14ed248d650e681ea0386c
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-12 14:01:42 +0100

    Create time-complexity-3.js

---
+++ time-complexity-3.js
@@ -0,0 +1,15 @@
+const boom = (n) =>  {
+  for (let i = 0; i < 3; i++) {
+    bam(n);
+  }
+  
+  for (let k = 0; k < 10000; k++) {
+    console.log(k);
+  }
+};
+
+const bam = (n) => {
+  for (let j = 0; j < m; j++) {
+    console.log(j);
+  }
+};
