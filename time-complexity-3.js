5df34994b12782218353fb76b457a4f293145390
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-16 06:42:07 +0100

    Update time-complexity-3.js

--- time-complexity-3.js
+++ time-complexity-3.js
@@ -1,15 +1,19 @@
 const boom = (n) =>  {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) { //first  = 3
     bam(n);
   }
   
-  for (let k = 0; k < 10000; k++) {
+  for (let k = 0; k < 10000; k++) { //second = 1
     console.log(k);
   }
 };
 
 const bam = (n) => {
-  for (let j = 0; j < m; j++) {
+  for (let j = 0; j < m; j++) { // first = n
     console.log(j);
   }
 };
+boom(10);
+
+//Time complexity 
+3n + 1 => O(n), //where n is the input number
