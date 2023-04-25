0158345d1de434cb68e99f4383affc460bb797e7
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-25 14:10:33 +0100

    Update reverse_string.js

--- Arrays and strings/reverse_string.js
+++ Arrays and strings/reverse_string.js
@@ -1,3 +1,4 @@
+//1.
 function reversedString(str) {
   let revStr = "";
   for (let i =str.length - 1; i >= 0; i-- ) {
@@ -9,3 +10,15 @@ function reversedString(str) {
 const input = 'hello';
 const output = reversedString(input);
 console.log(output);
+
+
+//2.
+const input = 'hello';
+const output = input.split('').map((char, index, array) => {
+  return array[array.length - index - 1];
+}).join('');
+console.log(output); // "olleh"
+//Breakdown
+const input = 'hello';
+const output = input.split("")
+console.log(output);// [ 'h', 'e', 'l', 'l', 'o' ]
