258056b9a21c2879f4dd6b3d30ef8049fb727f29
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-27 11:59:35 +0100

    Update array.js

--- Arrays and strings/array.js
+++ Arrays and strings/array.js
@@ -18,7 +18,40 @@ A dynamic array, on the other hand, can grow or shrink in size as needed.
 */
 // Example of a fixed-size array
 let arr = new Array(5); // creates an array with 5 undefined elements
+const fruits = new Array("Apple", "Banana");
+console.log(fruits)//[ 'Apple', 'Banana' ]
 
 // Example of a dynamic array
 let arr = []; // creates an empty array
 arr.push(1); // adds an element to the end of the array
+
+
+//3. What is the time complexity of accessing an element in an array? How does it compare to the time complexity of accessing an element in a linked list?
+/*
+The time complexity of accessing an element in an array is O(1), which means it takes constant time regardless of the size of the array. 
+In contrast, accessing an element in a linked list takes O(n) time in the worst case, where n is the length of the list.
+*/
+
+// Example of accessing an element in an array
+let arr = [1, 2, 3, 4, 5];
+let element = arr[2]; // gets the element at index 2 (which is 3)
+
+// Example of accessing an element in a linked list
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+let current = head;
+while (current !== null) {
+  if (current.value === 2) {
+    let element = current.value; // gets the element with value 2
+    break;
+  }
+  current = current.next;
+}
