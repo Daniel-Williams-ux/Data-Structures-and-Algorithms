7b87b60204bd480e63a34896a7118d4fdfbac623
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-30 11:16:46 +0100

    Update Remove_Items_Using_splice().js

--- Basic_Data_structures/Remove_Items_Using_splice().js
+++ Basic_Data_structures/Remove_Items_Using_splice().js
@@ -17,3 +17,8 @@ let newArray = array.splice(3, 2);
 //newArray has the value ['really', 'happy'].
 
 
+//We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1, 4)
+console.log(arr);//[ 2, 5, 2, 1 ]
