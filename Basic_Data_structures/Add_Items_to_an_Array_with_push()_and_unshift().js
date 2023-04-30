563449facadf58b3e4c29a93f2457cf70491b77b
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-30 09:07:09 +0100

    Create Add_Items_to_an_Array_with_push()_and_unshift().js

---
+++ Basic_Data_structures/Add_Items_to_an_Array_with_push()_and_unshift().js
@@ -0,0 +1,13 @@
+//We have defined a function, mixedNumbers, which we are passing an array as an argument. Modify the function by using push() 
+//and unshift() to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to the end so that the returned array contains representations of
+//the numbers 1-9 in order.
+
+
+function mixedNumbers(arr) {
+  let arr1 = arr.unshift('I', 2, 'three')
+  let arr2 = arr.push(7, 'VIII', 9)
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+//['I',     2, 'three', 'IV', 5, 'six', 7, 'VIII', 9]
