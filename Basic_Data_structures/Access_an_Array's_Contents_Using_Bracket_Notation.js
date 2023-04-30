353bc4bc4bb285a4a4d20de5966331577b9e858a
Daniel Williams <williamsoligodaniel@gmail.com>
2023-04-30 08:58:02 +0100

    Create Access_an_Array's_Contents_Using_Bracket_Notation.js

---
+++ Basic_Data_structures/Access_an_Array's_Contents_Using_Bracket_Notation.js
@@ -0,0 +1,7 @@
+//set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 2;
+// Only change code above this line
+console.log(myArray);//[ 'a', 2, 'c', 'd' ]
