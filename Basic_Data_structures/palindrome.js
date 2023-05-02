67c0885a1ca1f4f4dd16f08eeb8fa292019c233e
Daniel Williams <williamsoligodaniel@gmail.com>
2023-05-02 19:23:04 +0100

    Update palindrome.js

--- Basic_Data_structures/palindrome.js
+++ Basic_Data_structures/palindrome.js
@@ -11,3 +11,40 @@ const isPalindrome = function(x) {
 
 const output = isPalindrome('121');
 console.log(output);
+
+/*case 1    
+Input        
+x =
+121
+Output
+true
+Expected
+true 
+
+case 2
+Input
+x =
+-121
+Output
+false
+Expected
+false
+
+case 3
+Input
+x =
+10
+Output
+false
+Expected
+false
+*/
+
+
+//Optimizing the solution using edge case
+const isPalindrome = function(x) {
+  return x < 0 ? false : (x === +x.toString().split("").reverse().join(""))
+}
+
+const output = isPalindrome('121');
+console.log(output);
